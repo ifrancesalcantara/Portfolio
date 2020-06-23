8f4c36399c3342030ba625392579e0e7cd5fa4b0
Iván Francés <ifrancesalcantara@gmail.com>
2020-06-23 17:16:34 +0200

    project baseURL to app only

--- src/lib/services/project-service.js
+++ src/lib/services/project-service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL=process.env.REACT_APP_API_URL+"/project"
+const baseURL="https://ifrances-portfolio2.herokuapp.com/project"
 
 class projectService {
   constructor() {
